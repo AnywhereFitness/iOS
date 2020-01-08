ee8a3c3ee521226e82bea87e1c08349f71b4d026
brian <bfvilchez95@gmail.com>
2020-01-08 11:15:25 -0700

    add NetworkAPI file

---
+++ AnywhereFitness/AnywhereFitness/Networking/AnytimeFitnessAPI.swift
@@ -0,0 +1,13 @@
+//
+//  AnytimeFitnessAPI.swift
+//  AnywhereFitness
+//
+//  Created by brian vilchez on 1/8/20.
+//  Copyright © 2020 Brandi Bailey. All rights reserved.
+//
+
+import Foundation
+
+class AnytimeFitnessAPI {
+    
+}
